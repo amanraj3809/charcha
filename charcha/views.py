1528255641ab9461df4cd813370c3ca6da171153
Aman Raj <aman.raj@hashedin.com>
2017-04-24 01:54:56 +0530

    Serving service worker javascript as view

--- charcha/views.py
+++ charcha/views.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals, absolute_import
 from django.http.response import HttpResponse
 
+from django.views.decorators.cache import never_cache
+from django.template.loader import get_template
+
 def health_check(request):
     return HttpResponse("OK", status=200)
+
+
+@never_cache
+def charcha_serviceworker(request, js):
+    template = get_template('charcha-serviceworker.js')
+    html = template.render()
+    return HttpResponse(html, content_type="application/x-javascript")
